7b4fd60435d4a7b039fbd4f3382510eb5e35e1f4
Reginaldo Lopes <rgllopes@hotmail.com>
2023-01-22 23:18:50 -0300

    Fim secao 3: Comunicacao entre microservices

--- msavaliadorcredito/src/main/java/io/github/cursodsousa/msavaliadorcredito/application/AvaliadorCreditoController.java
+++ msavaliadorcredito/src/main/java/io/github/cursodsousa/msavaliadorcredito/application/AvaliadorCreditoController.java
@@ -1,12 +1,14 @@
 package io.github.cursodsousa.msavaliadorcredito.application;
 
+import io.github.cursodsousa.msavaliadorcredito.application.ex.DadosClienteNotFoundException;
+import io.github.cursodsousa.msavaliadorcredito.application.ex.ErroComunicacaoMicroservicesException;
+import io.github.cursodsousa.msavaliadorcredito.domain.model.DadosAvaliacao;
+import io.github.cursodsousa.msavaliadorcredito.domain.model.RetornoAvaliacaoCliente;
 import io.github.cursodsousa.msavaliadorcredito.domain.model.SituacaoCliente;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("avaliacoes-credito")
@@ -20,8 +22,25 @@ public class AvaliadorCreditoController {
     }
 
     @GetMapping(value = "situacao-cliente", params = "cpf")
-    public ResponseEntity<SituacaoCliente> consultaSituacaoCliente(@RequestParam("cpf") String cpf){
-        SituacaoCliente situacaoCliente = avaliadorCreditoService.obterSituacaoCliente(cpf);
-        return ResponseEntity.ok(situacaoCliente);
+    public ResponseEntity consultaSituacaoCliente(@RequestParam("cpf") String cpf){
+        try {
+            SituacaoCliente situacaoCliente = avaliadorCreditoService.obterSituacaoCliente(cpf);
+            return ResponseEntity.ok(situacaoCliente);
+        } catch (DadosClienteNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (ErroComunicacaoMicroservicesException e) {
+            return ResponseEntity.status(HttpStatus.resolve(e.getStatus())).body(e.getMessage());
+        }
+    }
+    @PostMapping
+    public ResponseEntity realizarAvaliacao(@RequestBody DadosAvaliacao dados) {
+        try {
+            RetornoAvaliacaoCliente retornoAvaliacaoCliente = avaliadorCreditoService.realizarAvaliacao(dados.getCpf(), dados.getRenda());
+            return ResponseEntity.ok(retornoAvaliacaoCliente);
+        } catch (DadosClienteNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (ErroComunicacaoMicroservicesException e) {
+            return ResponseEntity.status(HttpStatus.resolve(e.getStatus())).body(e.getMessage());
+        }
     }
 }
--- msavaliadorcredito/src/main/java/io/github/cursodsousa/msavaliadorcredito/application/AvaliadorCreditoService.java
+++ msavaliadorcredito/src/main/java/io/github/cursodsousa/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -1,26 +1,81 @@
 package io.github.cursodsousa.msavaliadorcredito.application;
 
-import io.github.cursodsousa.msavaliadorcredito.domain.model.DadosCliente;
-import io.github.cursodsousa.msavaliadorcredito.domain.model.SituacaoCliente;
+import feign.FeignException;
+import io.github.cursodsousa.msavaliadorcredito.application.ex.DadosClienteNotFoundException;
+import io.github.cursodsousa.msavaliadorcredito.application.ex.ErroComunicacaoMicroservicesException;
+import io.github.cursodsousa.msavaliadorcredito.domain.model.*;
+import io.github.cursodsousa.msavaliadorcredito.infra.clients.ICartoesResourceClient;
 import io.github.cursodsousa.msavaliadorcredito.infra.clients.IClienteResourceClient;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class AvaliadorCreditoService {
 
     private final IClienteResourceClient clientesClient;
-    public SituacaoCliente obterSituacaoCliente(String cpf){
-        //obter dados do do cliente - msclientes
-        //obter cartoes do cliente - mscartoes
+    private  final ICartoesResourceClient cartoesClient;
 
-        ResponseEntity<DadosCliente> dadosClienteResponse = clientesClient.dadosCliente(cpf);
+    public SituacaoCliente obterSituacaoCliente(String cpf) throws DadosClienteNotFoundException, ErroComunicacaoMicroservicesException {
+        try
+        {
+            //obter dados do do cliente - msclientes
+            ResponseEntity<DadosCliente> dadosClienteResponse = clientesClient.dadosCliente(cpf);
+            //obter cartoes do cliente - mscartoes
+            ResponseEntity<List<CartaoCliente>> cartoesResponse = cartoesClient.getCartoesByCliente(cpf);
 
-        return SituacaoCliente
-                .builder()
-                .cliente(dadosClienteResponse.getBody())
-                .build();
+            return SituacaoCliente
+                    .builder()
+                    .cliente(dadosClienteResponse.getBody())
+                    .cartoes(cartoesResponse.getBody())
+                    .build();
+        } catch (FeignException.FeignClientException e) {
+            int status = e.status();
+            if (HttpStatus.NOT_FOUND.value() == status) {
+                throw  new DadosClienteNotFoundException();
+            }
+            throw  new ErroComunicacaoMicroservicesException(e.getMessage(), status);
+        }
+    }
+
+    public RetornoAvaliacaoCliente realizarAvaliacao(String cpf, Long renda)
+            throws DadosClienteNotFoundException, ErroComunicacaoMicroservicesException {
+        try{
+            ResponseEntity<DadosCliente> dadosClienteResponse = clientesClient.dadosCliente(cpf);
+            ResponseEntity<List<Cartao>> cartaoResponse = cartoesClient.getCartoesRendaAteh(renda);
+
+            List<Cartao> cartaos = cartaoResponse.getBody();
+            var listaCartoesAprovados = cartaos.stream().map(cartao -> {
+                DadosCliente dadosCliente = dadosClienteResponse.getBody();
+
+                BigDecimal limiteBasico = cartao.getLimiteBasico();
+                BigDecimal rendaBD = BigDecimal.valueOf(renda);
+                BigDecimal idadeBD = BigDecimal.valueOf(dadosCliente.getIdade());
+                var fator = idadeBD.divide(BigDecimal.valueOf(10));
+                BigDecimal limiteAprovado = fator.multiply(limiteBasico);
+
+                CartaoAprovado aprovado = new CartaoAprovado();
+                aprovado.setCartao(cartao.getNome());
+                aprovado.setBandeira(cartao.getBandeira());
+                aprovado.setLimiteAprovado(limiteAprovado);
+
+                return aprovado;
+            }).collect(Collectors.toList());
+
+            return new RetornoAvaliacaoCliente(listaCartoesAprovados);
+
+        }catch (FeignException.FeignClientException e) {
+            int status = e.status();
+            if (HttpStatus.NOT_FOUND.value() == status) {
+                throw  new DadosClienteNotFoundException();
+            }
+            throw  new ErroComunicacaoMicroservicesException(e.getMessage(), status);
+        }
     }
 }
